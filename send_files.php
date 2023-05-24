399113e142c9dc33bf7a605927bd943c854df64f
luizalvesot <alvesluizotavio23@gmail.com>
2023-05-24 16:40:52 -0300

    Criação do arquivo responsável por enviar as gravações finalizadas para a máquina remota e serem localizadas pelo LINK. Done issue #5

---
+++ send_files.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php -q
+<?php
+
+require __DIR__ . '/geral.php';
+
+$base_path = '/var/spool/asterisk/monitor/';
+
+
+/**
+ * 
+ * Upload chamadas de saída do Exactspotter para https://nubbi.easypabx.com.br/var/www/exactspotter-recordings/
+ * 
+ **/
+
+$query = "SELECT * FROM call_out_exactspotter WHERE voice_opened IS NOT NULL AND recording_sent IS NULL";
+
+$calls = selectAll($query);
+
+if (!empty($calls)) {
+    foreach ($calls as $call) {
+        
+        $result = sendFiles($call['recordingfile']);
+
+        if (file_exists($base_path . $call['recordingfile'])) {
+
+            $query = "UPDATE call_out_exactspotter SET recording_sent=NOW() WHERE id={$call['id']}";
+            updateData($query);
+        }
+    }
+}
